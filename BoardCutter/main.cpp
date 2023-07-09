81e6ba1d3dcc0785216f87ca891243f02faf4133
e0035165 <eeenginmaster95@gmail.com>
2023-07-09 20:04:49 +0800

    Initial Commit

---
+++ BoardCutter/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BoardCutter
+//
+//  Created by Sart Way on 9/7/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
